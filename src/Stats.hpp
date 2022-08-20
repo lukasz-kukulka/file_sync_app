2055d457226b087ed8df561baea82095287fefaf
lukasz-kukulka <qqla83@gmail.com>
2022-08-20 11:13:47 +0100

    add function to compare fileinfo

--- src/Stats.hpp
+++ src/Stats.hpp
@@ -2,6 +2,7 @@
 
 #include "FileInfo.hpp"
 
+#include <iostream>
 #include <string>
 #include <filesystem>
 
@@ -21,6 +22,49 @@ namespace SyncApp {
         Exit
     };
 
+    enum class CompareOption {
+        Equal,
+        Different,
+        Greater,
+        Less,
+    };
 
+    CompareOption getCompareOption( FileInfo* const lhs, FileInfo* const rhs ) {
+        if( ( not lhs || not rhs ) || ( lhs->getPath() != rhs->getPath() ) ) {
+            return CompareOption::Different;
+        }
+        if ( lhs->getAbsolutePath() == rhs->getAbsolutePath() ) {
+            return CompareOption::Equal;
+        } else {
+            if ( lhs->getModTime() != rhs->getModTime() ) {
+                if ( lhs->getModTime() != rhs->getModTime() && lhs->getModTime() > rhs->getModTime() ) {
+                    return CompareOption::Greater;
+                } else if ( lhs->getModTime() != rhs->getModTime() && lhs->getModTime() < rhs->getModTime() ) {
+                    return CompareOption::Less;
+                }
+            } 
+        }
+    }
+
+    template<typename... Args>
+    FileInfo* compareFilesInfo( const Args*... args ) {
+        FileInfo* newest_file = nullptr;
+        for (auto const& info : args... ) {
+            auto const compare_result = getCompareOption( newest_file, info );
+            switch ( compare_result ) {
+                case CompareOption::Different :
+                case CompareOption::Equal :
+                    return nullptr;
+                case CompareOption::Greater :
+                    return newest_file;
+                case CompareOption::Less :
+                    return info;
+                default:    
+                    std::cout << "ERROR WRONG VALUE IN COMPARE FUNCTION\n"; 
+                    return nullptr;
+            }
+        }
+        return nullptr;
+    }
 }
 
