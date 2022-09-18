c898e56ed8aabf00184085dfddcce8fa0bcbb3c0
lukasz-kukulka <qqla83@gmail.com>
2022-09-18 12:55:23 +0100

    print menu fun

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -5,5 +5,9 @@ Menu::Menu() {
 }
 
 Menu::printMenu() const {
-    
+    int menu_num { 1 };
+    std::cout << "SYNC APP\n";
+    std::cout << menu_num++ << ". Synchronize machines\n";
+    std::cout << menu_num++ << ". Settings\n";
+    std::cout << menu_num++ << ". Exit\n";
 }
