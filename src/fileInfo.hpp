4a4ba5c9bfa20a581746c9e1ec99f47a67b46353
lukasz-kukulka <qqla83@gmail.com>
2022-12-18 03:26:06 +0000

    add spaceship operator

--- src/fileInfo.hpp
+++ src/fileInfo.hpp
@@ -7,9 +7,12 @@ namespace fs = std::filesystem;
 class FileInfo {
 public:
     FileInfo( fs::directory_entry directory_entry);
-    bool operator==( const FileInfo& lhs ) const;
+    auto operator<=>( const FileInfo& lhs ) const = default;
+    // bool operator==( const FileInfo& lhs ) const;
 private:
     std::time_t setSystemTime( fs::file_time_type const& ftime );
+
+
     std::time_t system_time_{};
     fs::directory_entry directory_entry_{};
 };
